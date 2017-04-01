30f3ed3bed136373f6cf004fef4c8996da980814
tongjiaolong <tongjiaolong@yeah.net>
2017-04-01 11:36:38 +0800

    简单全局memory驱动，支持一个设备

--- drivers/test/globalmem.c
+++ drivers/test/globalmem.c
@@ -17,25 +17,24 @@
 #define MEM_CLEAR   0x1
 #define GLOBALMEM_MAJOR 230
 #define DEVICE_NUM  10
-static  int globalmem_major = GLOBALMEM_MAJOR;
 
-module_param(globalmem_major,int,S_IRUGO);
+static  int globalmem_major = GLOBALMEM_MAJOR;         // 设备驱动主设备号
 
-struct globalmem_dev{
+module_param(globalmem_major,int,S_IRUGO);             //模块参数，可传入主设备号
+
+struct globalmem_dev{                                  //设备结构体，封装了cdev和全局memory
        struct cdev cdev;
 	   unsigned char mem[GLOBALMEM_SIZE];
 };
 
 
-struct globalmem_dev *globalmem_devp;
+struct globalmem_dev *globalmem_devp;                  //声明一个全局设备指针
 
 
 
 static int globalmem_open(struct inode *inode,struct file *filep){
 
-	//struct globalmem_dev *dev=container_of(inode->i_cdev,struct globalmem_dev,cdev);
-	//filep->private_data = dev;
-	filep->private_data = globalmem_devp;
+	filep->private_data = globalmem_devp;          //设备打开，设备指针指向file的私有数据private_data，驱动文件通过访问priavte_data就能访问设备文件
 	return 0;
 }
 
@@ -172,48 +171,34 @@ static int __init globalmem_init(void){
 
 	   dev_t devno;
        int   ret,i;
-	   devno =MKDEV(globalmem_major,0);
+	   devno =MKDEV(globalmem_major,0);                       //生成设备号
 
 	   if(globalmem_major)
-	   	register_chrdev_region(devno,1,"globalmem");
-	   	//register_chrdev_region(devno,DEVICE_NUM,"globalmem");
+	   	register_chrdev_region(devno,1,"globalmem");      //手动注册设备号
 	   else{
-	   	alloc_chrdev_region(&devno,0,1,"globalmem");
-	   	//alloc_chrdev_region(&devno,0,DEVICE_NUM,"globalmem");
-		globalmem_major = MAJOR(devno);
+	   	alloc_chrdev_region(&devno,0,1,"globalmem");      //自动注册设备号(系统自动分配设备号)
+		globalmem_major = MAJOR(devno);                   //获得主设备号
 	   }
 
-       globalmem_devp=kzalloc(sizeof(struct globalmem_dev),GFP_KERNEL);
-       //globalmem_devp=kzalloc(sizeof(struct globalmem_dev)*DEVICE_NUM,GFP_KERNEL);
+           globalmem_devp=kzalloc(sizeof(struct globalmem_dev),GFP_KERNEL);   //为设备分配内存空间
 	   if(!globalmem_devp){
 		  ret = -ENOMEM;
 		  goto fail_alloc; 
 	   	}
-	   globalmem_setup_cdev(globalmem_devp,0);
-       //for(i=0;i<DEVICE_NUM;i++){   	
-	//		globalmem_setup_cdev(globalmem_devp+i,i);
-		
-	//   	}
+	   globalmem_setup_cdev(globalmem_devp,0);                  //初始化设备并向系统添加一个cdev，完成设备的注册 
+
 	   return 0;
 fail_alloc:
 	   unregister_chrdev_region(devno,1);
-	   //unregister_chrdev_region(devno,DEVICE_NUM);
 	   return -1;
 	   	
 	
 }
 
 
-static void __exit globalmem_exit(void){
-	cdev_del(&(globalmem_devp)->cdev);
-	//int i;
-	//for(i=0;i<DEVICE_NUM;i++){
-    	//cdev_del(&(globalmem_devp+i)->cdev);
-	//}
-	
-	unregister_chrdev_region(MKDEV(globalmem_major,0),1);
-	//unregister_chrdev_region(MKDEV(globalmem_major,0),DEVICE_NUM);
-
+static void __exit globalmem_exit(void){                          //从系统中删除一个cdev设备，并回收设备号，完成设备卸载
+	cdev_del(&(globalmem_devp)->cdev);	
+	unregister_chrdev_region(MKDEV(globalmem_major,0),1);      
 	kfree(globalmem_devp);
 }
 
